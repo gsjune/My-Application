5391f134734e5f20487330686f6cfe9397549645
Hongchul Kim <gsjune@naver.com>
2017-08-03 06:40:26 +0900

    first commit

---
+++ src/main/java/com/hckim/myapplication/CoffeeActivity.java
@@ -0,0 +1,31 @@
+package com.hckim.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CoffeeActivity extends AppCompatActivity {
+
+    // 메인
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 레이아웃 설정
+        setContentView(R.layout.activity_coffee); // 레퍼런스
+
+        // xml에 있는 View의 레퍼런스를 가져오는 방법
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text);
+
+        quantityTextView.setText("10");
+    }
+
+    public void minusButtonClicked(View view) {
+        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
+    }
+
+    public void plusButtonClicked(View view) {
+    }
+}
