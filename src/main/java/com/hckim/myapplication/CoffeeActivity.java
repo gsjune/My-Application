0ffe18e942ae472008fdea545cf96c294637cf2c
Hongchul Kim <gsjune@naver.com>
2017-08-03 15:18:11 +0900

    coffee 주문 앱 - 리팩토링

--- src/main/java/com/hckim/myapplication/CoffeeActivity.java
+++ src/main/java/com/hckim/myapplication/CoffeeActivity.java
@@ -9,9 +9,13 @@ import java.text.DecimalFormat;
 
 public class CoffeeActivity extends AppCompatActivity {
 
+    public static final int MIN_QUANTITY = 1; // D(1)의 결과
+    public static final int DEFAULT_QUANTITY = 1; // D(2)의 결과
+    public static final int COFFEE_PRICE = 3000; // D(3)의 결과
     private TextView mQuantityTextView;
     private TextView mPriceTetView; // B(1)
-    private int mQuantity = 1; // (2)''
+    //    private int mQuantity = 1;
+    private int mQuantity = DEFAULT_QUANTITY; // (2)'' D(2)
 
     private DecimalFormat mFormat = new DecimalFormat("#,##0"); // C(1)
 
@@ -30,26 +34,57 @@ public class CoffeeActivity extends AppCompatActivity {
 //        Button minusButton = (Button) findViewById(R.id.minus_button); // onClick 방법 쓴다
 //        Button plusButton = (Button) findViewById(R.id.plus_button);
 
+
+////        quantityTextView.setText("10"); // 10 테스트. setText()는 텍스트 바꿈.
+//        mQuantityTextView.setText("" + mQuantity); // (2)''' "" + mQuantity
+//        mPriceTetView.setText(mFormat.format(mQuantity * COFFEE_PRICE) + "원"); // B(3) C(4) D(3)
+//    }
+//
+//    public void minusButtonClicked(View view) { // (1) 전역변수 필요
+////        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
+//        mQuantity--;
+////        if (mQuantity < 1) { // (5)
+////            mQuantity = 1;
+////        }
+//        if (mQuantity < MIN_QUANTITY) { // D(1) 상수 Find Action constant
+//            mQuantity = MIN_QUANTITY;
+//        }
+//        mQuantityTextView.setText("" + mQuantity); // (3) (2)''' 복사
+////        mPriceTetView.setText(mQuantity * 3000 + "원"); // B(4)
+//        mPriceTetView.setText(mFormat.format(mQuantity * COFFEE_PRICE) + "원"); // C(2)
+//    }
+//
+//    public void plusButtonClicked(View view) {
+//        mQuantity++;
+//        mQuantityTextView.setText("" + mQuantity); // (4)
+////        mPriceTetView.setText(mQuantity * 3000 + "원"); // B(5)
+//        mPriceTetView.setText(mFormat.format(mQuantity * COFFEE_PRICE) + "원"); // C(3)
+//    }
+
+
 //        quantityTextView.setText("10"); // 10 테스트. setText()는 텍스트 바꿈.
-        mQuantityTextView.setText("" + mQuantity); // (2)''' "" + mQuantity
-        mPriceTetView.setText(mFormat.format(mQuantity * 3000) + "원"); // B(3) C(4)
+        display(); // D(4)' D(4)의 결과
+    }
+
+    private void display() {
+        mQuantityTextView.setText("" + mQuantity);
+        mPriceTetView.setText(mFormat.format(mQuantity * COFFEE_PRICE) + "원"); // D(4) Alt Enter method
     }
 
     public void minusButtonClicked(View view) { // (1) 전역변수 필요
 //        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
         mQuantity--;
-        if (mQuantity < 1) { // (5)
-            mQuantity = 1;
+//        if (mQuantity < 1) { // (5)
+//            mQuantity = 1;
+//        }
+        if (mQuantity < MIN_QUANTITY) { // D(1) 상수 Find Action constant
+            mQuantity = MIN_QUANTITY;
         }
-        mQuantityTextView.setText("" + mQuantity); // (3) (2)''' 복사
-//        mPriceTetView.setText(mQuantity * 3000 + "원"); // B(4)
-        mPriceTetView.setText(mFormat.format(mQuantity * 3000) + "원"); // C(2)
+        display(); // D(4)'' D(4)의 결과
     }
 
     public void plusButtonClicked(View view) {
         mQuantity++;
-        mQuantityTextView.setText("" + mQuantity); // (4)
-//        mPriceTetView.setText(mQuantity * 3000 + "원"); // B(5)
-        mPriceTetView.setText(mFormat.format(mQuantity * 3000) + "원"); // C(3)
+        display(); // D(4)''' D(4)의 결과
     }
 }
