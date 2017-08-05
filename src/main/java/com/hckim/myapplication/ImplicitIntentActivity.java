a78c8dd85a231d4bb0771bce6d945673385b6d10
Hongchul Kim <gsjune@naver.com>
2017-08-06 08:13:55 +0900

    암시적 인텐트: 텍스트 전송

--- src/main/java/com/hckim/myapplication/ImplicitIntentActivity.java
+++ src/main/java/com/hckim/myapplication/ImplicitIntentActivity.java
@@ -44,4 +44,19 @@ public class ImplicitIntentActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    public void sendMessage(View view) {
+        EditText messageEditText = (EditText) findViewById(R.id.message_edit);
+
+        sendTextMessage(messageEditText.getText().toString());
+    }
+
+    public void sendTextMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
