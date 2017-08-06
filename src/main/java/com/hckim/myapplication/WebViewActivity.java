4b72e006836bb2d83713c296d7bb280c9fbec093
Hongchul Kim <gsjune@naver.com>
2017-08-06 13:55:53 +0900

    웹뷰에 JavaScript 사용한 페이지를 볼 수 있도록

--- src/main/java/com/hckim/myapplication/WebViewActivity.java
+++ src/main/java/com/hckim/myapplication/WebViewActivity.java
@@ -19,14 +19,28 @@ public class WebViewActivity extends AppCompatActivity {
 
         mWebView = (WebView) findViewById(R.id.web_view); // (3)
         mUrlEditext = (EditText) findViewById(R.id.url_edit); // (4) Alt Enter Create field 'mUrlEditText'
-    }
-
-    public void showWebPage(View view) { // (1)
-        String url = mUrlEditext.getText().toString();
-        mWebView.loadUrl(url); // (6) (8) 괄호 안
 
         // 요거 해 줘야 한다
-        mWebView.setWebViewClient(new WebViewClient()); // (9) 중요
+        mWebView.setWebViewClient(new WebViewClient()); // D(1) 여기에서 한 번만 해 주면 된다
+        // 자바스크립트 사용하는 페이지를 볼 수 있게
+        mWebView.getSettings().setJavaScriptEnabled(true); // D(2) D(1)과 D(2) 두 줄은 필수
+    }
+//    // 기본
+//    public void showWebPage(View view) { // (1)
+//        String url = mUrlEditext.getText().toString();
+//        mWebView.loadUrl(url); // (6) (8) 괄호 안
+//
+//        // 요거 해 줘야 한다
+//        mWebView.setWebViewClient(new WebViewClient()); // (9) 중요
+    // 응용
+    public void showWebPage(View view) { // C(1)
+        String url = mUrlEditext.getText().toString();
+
+        if (url.startsWith("http://") || url.startsWith("https://")) {
+            mWebView.loadUrl(url);
+        } else {
+            mWebView.loadUrl("https://" + url);
+        }
     }
 
     public void goBack(View view) { // B(1)
