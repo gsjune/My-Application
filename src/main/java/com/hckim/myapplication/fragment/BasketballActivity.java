625d1ed3d9c5ab0e79f67bcb83ba9a63a5378e3e
Hongchul Kim <gsjune@naver.com>
2017-08-12 01:27:02 +0900

    fragment\BasketballActivity.java

--- src/main/java/com/hckim/myapplication/fragment/BasketballActivity.java
+++ src/main/java/com/hckim/myapplication/fragment/BasketballActivity.java
@@ -2,14 +2,29 @@ package com.hckim.myapplication.fragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hckim.myapplication.R;
 
 public class BasketballActivity extends AppCompatActivity {
 
+    private BasketScoreFragment mATeamFragment;
+    private BasketScoreFragment mBTeamFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basketball2);
+
+
+        mATeamFragment = (BasketScoreFragment) getSupportFragmentManager().findFragmentById(R.id.frag_team_a); // Alt Enter Cast to ... Enter
+        mBTeamFragment = (BasketScoreFragment) getSupportFragmentManager().findFragmentById(R.id.frag_team_b);
+
+        mBTeamFragment.setTeamName("Team B");
+    }
+
+    public void onResetButtonClicked(View view) {
+        mATeamFragment.reset();
+        mBTeamFragment.reset();
     }
 }
