8733099ebb8e81d2bebfaba64ba90f054afc8b25
Hongchul Kim <gsjune@naver.com>
2017-08-11 23:49:31 +0900

    fragment_basket_score.xml <- activity_basketball.xml

---
+++ src/main/java/com/hckim/myapplication/fragment/BasketballActivity.java
@@ -0,0 +1,15 @@
+package com.hckim.myapplication.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hckim.myapplication.R;
+
+public class BasketballActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball2);
+    }
+}
