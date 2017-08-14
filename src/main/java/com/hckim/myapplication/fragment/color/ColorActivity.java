2399a40789fcc1771aa1f384af6e638d22c1722d
Hongchul Kim <gsjune@naver.com>
2017-08-14 23:53:24 +0900

    팩토리 패턴: newInstance

--- src/main/java/com/hckim/myapplication/fragment/color/ColorActivity.java
+++ src/main/java/com/hckim/myapplication/fragment/color/ColorActivity.java
@@ -1,7 +1,9 @@
 package com.hckim.myapplication.fragment.color;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hckim.myapplication.R;
 
@@ -11,5 +13,39 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+    }
+
+//    public void onCreateFragmentButtonClicked(View view) {
+//        // 새로운 프래그먼트를 생성
+//
+//        ColorFragment fragment = new ColorFragment();
+//
+//        // R.id.container 영역에 프래그먼트를 교체하겠다
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+//    }
+    public void onCreateRedFragmentButtonClicked(View view) {
+
+        ColorFragment fragment = ColorFragment.newInstance(Color.RED);
+
+        // R.id.container 영역에 프래그먼트를 교체하겠다
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    public void onCreateGreenFragmentButtonClicked(View view) {
+        ColorFragment fragment = ColorFragment.newInstance(Color.GREEN);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    public void onCreateBlueFragmentButtonClicked(View view) {
+        ColorFragment fragment = ColorFragment.newInstance(Color.BLUE);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    public void onCreateRandomFragmentButtonClicked(View view) {
+        ColorFragment fragment = new ColorFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 }
--- src/main/java/com/hckim/myapplication/fragment/color/ColorFragment.java
+++ src/main/java/com/hckim/myapplication/fragment/color/ColorFragment.java
@@ -1,7 +1,9 @@
 package com.hckim.myapplication.fragment.color;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,16 +11,27 @@ import android.view.ViewGroup;
 
 import com.hckim.myapplication.R;
 
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ColorFragment extends Fragment {
 
-
+    // 프래그먼트는 반드시 빈 생성자만 있어야 한다
     public ColorFragment() {
         // Required empty public constructor
     }
 
+    public static ColorFragment newInstance(int color) { // B(1) new... Enter. B(2) () 안 int color
+
+        Bundle args = new Bundle();
+        args.putInt("color", color); // B(3)
+
+        ColorFragment fragment = new ColorFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,8 +40,18 @@ public class ColorFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_color, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) { // B(1) 색깔 랜덤으로 onvi... Enter
+        super.onViewCreated(view, savedInstanceState);
+
+        int r = new Random().nextInt(256);
+        int g = new Random().nextInt(256);
+        int b = new Random().nextInt(256);
+        int color = Color.argb(255, r, g, b);
+        view.setBackgroundColor(color);
+    }
+
     public void setColor(int color) {
         getView().setBackgroundColor(color);
     }
-
 }
